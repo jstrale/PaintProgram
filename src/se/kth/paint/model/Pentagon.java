5ff79f265ea6460f35b4dc595f5091993e7d6b2b
helenalinden <pkhli@kth.se>
2014-03-08 14:45:42 +0100

    added pentagon shape

---
+++ src/se/kth/paint/model/Pentagon.java
@@ -0,0 +1,39 @@
+/**
+ * Description: Represents a shape in the form of a pentagon
+ * 
+ * @author Helena Lindén
+ * @since 2014-03-08
+ * 
+ */
+package se.kth.paint.model;
+
+import java.awt.Graphics2D;
+
+public class Pentagon extends Shape {
+
+	@Override
+	protected void drawShape(Graphics2D g2) {
+		g2.drawPolygon(getXPoints(), getYPoints(), 5);
+	}
+
+	@Override
+	protected void drawFilledShape(Graphics2D g2) {
+		g2.fillPolygon(getXPoints(), getYPoints(), 5);
+	}
+
+	@Override
+	public Shape copy() {
+		return new Pentagon();
+	}
+	
+	private int[] getXPoints() {
+		int x = this.getPos().getX();
+		return new int[]{x, x + Shape.SIZE/2, x + Shape.SIZE/4, x - Shape.SIZE/4, x - Shape.SIZE/2};
+	}
+	
+	private int[] getYPoints() {
+		int y = this.getPos().getY();
+		return new int[]{y, y + Shape.SIZE/3, y + Shape.SIZE, y + Shape.SIZE, y + Shape.SIZE/3};
+	}
+
+}
--- src/se/kth/paint/model/handlers/ShapeHandler.java
+++ src/se/kth/paint/model/handlers/ShapeHandler.java
@@ -4,6 +4,7 @@ package se.kth.paint.model.handlers;
 import java.util.ArrayList;
 
 import se.kth.paint.model.Oval;
+import se.kth.paint.model.Pentagon;
 import se.kth.paint.model.Rectangle;
 import se.kth.paint.model.Shape;
 import se.kth.paint.model.Triangle;
@@ -17,6 +18,7 @@ public class ShapeHandler {
 		shapes.add(new Oval());
 		shapes.add(new Rectangle());
 		shapes.add(new Triangle());
+		shapes.add(new Pentagon());
 		
 		return shapes;
 	}
